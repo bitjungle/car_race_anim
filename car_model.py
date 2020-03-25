aae659437d45548091dedbc4d868626d8bde816d
bitjungle <devel@bitjungle.com>
2020-03-25 09:46:53 +0100

    Initial commit

---
+++ car_model.py
@@ -0,0 +1,64 @@
+''' Pygame Car Model
+
+Copyright (C) 2020 BITJUNGLE Rune Mathisen
+This code is licensed under a GPLv3 license 
+See http://www.gnu.org/licenses/gpl-3.0.html 
+'''
+
+import math
+import pygame
+
+class Car(pygame.sprite.Sprite):
+    '''A simple car model'''
+    def __init__(self, imagefile, speed=0, direction=[0, 0]):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(imagefile)
+        self.rect = self.image.get_rect()
+        self._speed = speed
+        self._direction = direction
+        self._odometer = 0
+
+    def set_xpos(self, pos):
+        self.rect.left = pos 
+
+    def get_xpos(self):
+        return self.rect.left
+
+    def set_ypos(self, pos):
+        self.rect.top = pos 
+
+    def get_ypos(self):
+        return self.rect.top
+
+    def set_speed(self, speed):
+        self._speed = speed
+
+    def get_speed(self):
+        return self._speed
+
+    def get_odometer(self):
+        return self._odometer
+
+    def set_direction(self, direction):
+        '''Set the car direction
+
+        The direction is given as a list:
+        [1, 0] : moving to the right (0 deg)
+        [-1, 0] : moving to the left (180 deg)
+        [0, -1] : moving up (90 deg)
+        [0, 1] : moving down (270 deg)
+        '''
+        self._direction = direction
+
+    def move(self):
+        move_x = self._direction[0]*self._speed
+        move_y = self._direction[1]
+        self._odometer += math.sqrt(move_x**2 + move_y**2) # Pythagoras
+        self.rect = self.rect.move([move_x, move_y])
+
+    def turn(self):
+        self.image = pygame.transform.flip(self.image, True, False)
+        self._direction[0] *= -1
+
+    def colliderect(self, rect):
+        return self.rect.colliderect(rect)
---
+++ car_race.py
@@ -0,0 +1,104 @@
+''' Pygame Car Race Animation
+
+Simple animation to demonstrate animation in pygame, 
+and to give my students a starting point for making new animations.
+
+Copyright (C) 2020 BITJUNGLE Rune Mathisen
+This code is licensed under a GPLv3 license 
+See http://www.gnu.org/licenses/gpl-3.0.html 
+'''
+
+import sys
+import random
+import pygame
+import car_model as car
+
+pygame.init()
+#pygame.font.init()
+
+DISPLAY_WIDTH = 1024
+DISPLAY_HEIGHT = 768
+
+FPS = 25
+MAX_CARS = 14
+MAX_CAR_MODELS = 8
+MAX_SPEED = 10
+
+WHITE = pygame.Color(255, 255, 255)
+LIGHTGRAY = pygame.Color(211, 211, 211)
+SILVER = pygame.Color(192, 192, 192)
+DARKGRAY = pygame.Color(169, 169, 169)
+GRAY = pygame.Color(128, 128, 128)
+BLACK = pygame.Color(0, 0, 0)
+RED = pygame.Color(255, 0, 0)
+
+screen = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+pygame.display.set_caption('Car Race')
+myfont = pygame.font.SysFont(pygame.font.get_default_font(), 72)
+
+cars = [None]*MAX_CARS # list for storage of car objects
+lanes = [None]*MAX_CARS # list of lane numbers
+
+
+for i in range(0, MAX_CARS): # generating car objects
+    car_img = 'car_' + str(random.randint(1, MAX_CAR_MODELS)) + '.png'
+    car_speed = random.randint(1, MAX_SPEED)
+    cars[i] = car.Car(car_img, car_speed, [-1, 0])
+    cars[i].set_ypos(50 + i * 50)
+    cars[i].set_xpos(DISPLAY_WIDTH)
+    lanes[i] = myfont.render(str(i+1), False, LIGHTGRAY)
+
+racing = True
+leading_car = -1
+last_car = -1
+
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+
+    last_car_dist = DISPLAY_WIDTH*2
+    leading_car_dist = 0
+    
+    screen.fill(SILVER) # re-painting the screen
+
+    if racing:
+        for i in range(0, MAX_CARS):
+            # move cars and paint them on the screen
+            screen.blit(lanes[i],(DISPLAY_WIDTH-100, (i+1)*50))
+            cars[i].move()
+            screen.blit(cars[i].image, cars[i].rect)
+
+            # new speed is the mean of prior speed and a random number
+            cars[i].set_speed((random.randint(0, MAX_SPEED)+cars[i].get_speed()) // 2)
+
+            if cars[i].get_xpos() < 0:
+                # car has reached left edge of screen, turning it
+                cars[i].turn()
+                cars[i].set_xpos(1) # move the car 1px away from edge
+
+            if cars[i].get_odometer() < last_car_dist:
+                # keeping track of the last car in the field
+                last_car_dist = cars[i].get_odometer()
+                last_car = i
+            elif cars[i].get_odometer() > leading_car_dist:
+                # keeping track of the first car in the field
+                leading_car_dist = cars[i].get_odometer()
+                leading_car = i
+            else:
+                pass
+
+        #print('Leading #{}  dist: {} - Last #{}  dist: {}'.format(leading_car, leading_car_dist, last_car, last_car_dist))
+
+        if cars[leading_car].get_xpos() >= DISPLAY_WIDTH: 
+            # last car has finished the race
+            racing = False
+            text = 'The race is over, car #{} won'.format(leading_car+1)
+        else:
+            text = 'Car #{} is in the lead'.format(leading_car+1)
+    
+    textsurface = myfont.render(text, False, GRAY)
+    screen.blit(textsurface,(0, 0))
+    pygame.display.flip()
+    clock.tick(FPS)
